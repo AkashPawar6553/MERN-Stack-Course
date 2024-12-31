fd352ccb20e38e1ff2f506d880c0e5a69517cdde
AkashPawar6553 <Akashpawar6553@gmail.com>
2024-12-31 23:43:24 +0530

    HariomInnovations Project

---
+++ React.js/hariominnovations/src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ContactUs from "./Pages/ContactUS";
+
+function App() {
+  return (
+    <div >
+      <ContactUs />
+    </div>
+  );
+}
+
+export default App;
